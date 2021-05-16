bc631a41d8057a0b87e8e8e66ee00af8d023cacf
Bostanova <bostanzuhra@gmail.com>
2021-05-16 19:30:26 +0300

    add ft_memccpy.c

---
+++ ft_memccpy.c
@@ -0,0 +1,23 @@
+#include "libft.h"
+
+void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
+{
+	size_t		i;
+	char		*cast_dst;
+	const char	*cast_src;
+
+	cast_dst = (char *)dst;
+	cast_src = (char *)src;
+	i = 0;
+	while (i < n)
+	{
+		cast_dst[i] = cast_src[i];
+		if (cast_src[i] == c)
+			{
+				dst = (void *)cast_dst;
+				return (dst + i + 1);
+			}
+		i++;
+	}
+	return (0);
+}
