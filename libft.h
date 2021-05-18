3140901e31db20f11d10d72d08def4c204d6b425
Elenor Erika <eerika@ga-l5.msk.21-school.ru>
2021-05-18 15:46:57 +0300

    add ft_atoi.h

--- libft.h
+++ libft.h
@@ -44,5 +44,6 @@ void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_itoa(int n);
+int		ft_atoi(const char *str);
 
 #endif
