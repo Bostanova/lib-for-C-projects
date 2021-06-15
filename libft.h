e35d286a71f0fe1c48dd377e587c06e88588ba29
Elenor Erika <eerika@oa-c3.msk.21-school.ru>
2021-06-15 20:02:59 +0300

    add ft_lst* files

--- libft.h
+++ libft.h
@@ -4,6 +4,12 @@
 # include <unistd.h>
 # include <stdlib.h>
 
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}	t_list;
+
 void	ft_putchar(char c);
 void	ft_putendl(char *str);
 int		ft_putstr(char *str);
@@ -46,5 +52,10 @@ void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_itoa(int n);
 int		ft_atoi(const char *str);
+t_list	*ft_lstnew(void *content);
+int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
+void	ft_lstadd_front(t_list **lst, t_list *new);
+void	ft_lstadd_back(t_list **lst, t_list *new);
 
 #endif
